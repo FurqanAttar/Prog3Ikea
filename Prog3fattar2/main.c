e3deac45bcb80dee42c31c28d91385c90ddf07aa
Furqan Attar <furqanattar96@gmail.com>
2016-09-26 12:37:19 -0500

    Initial Commit

---
+++ Prog3fattar2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Prog3fattar2
+//
+//  Created by Furqan Attar on 9/26/16.
+//  Copyright © 2016 Furqan Attar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
